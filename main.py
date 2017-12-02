5359849646a054cc807317c009e6f3b6307184ca
NicoleMatos <nicole.matos3@upr.edu>
2017-12-02 18:02:38 -0400

    including in main searches by id or name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from handler.supplier import SupplierHandler
-#from handler.client import ClientHandler
+from handler.client import ClientHandler
 from handler.transaction import TransactionHandler
 from handler.announcement import AnnouncementHandler
 from handler.request import RequestHandler
@@ -22,11 +22,11 @@ def home():
 def getAllSuppliers():
     return SupplierHandler().getAllSuppliers()
 
-#@app.route('/clients/')
-#def getAllClients():
-#    return ClientHandler().getAllClients()
+@app.route('/clients/')
+def getAllClients():
+    return ClientHandler().getAllClients()
 
-@app.route('/transactions/')
+@app.route('/transaction/')
 def getAllTransactions():
     return TransactionHandler().getAllTransactions()
 
@@ -49,6 +49,42 @@ def getAllCCards():
 @app.route('/users/')
 def getAllUsers():
     return UserHandler().getAllUsers()
+
+@app.route('/client/<int:c_id>/')
+def getClientByID():
+    return ClientHandler().getClientByID()
+
+@app.route('/client/<int:c_id>/suppliers')
+def getSuppliersByClientID():
+    return ClientHandler().getSuppliersByClientID()
+
+@app.route('/client/<int:c_id>/transastions')
+def getTransactionsByClientID():
+    return ClientHandler().getTransactionsByClientID()
+
+@app.route('/client/<int:c_id>/requests')
+def getRequestsByClientID():
+    return ClientHandler().getRequestsByClientID()
+
+@app.route('/client/<int:c_id>/creditcard')
+def getCreditCardsByClientID():
+    return ClientHandler().getCreditCardsByClientID()
+
+@app.route('/client/<c_name>/suppliers')
+def getSuppliersByClientName():
+    return ClientHandler().getSuppliersByClientID()
+
+@app.route('/client/<c_name>/transastions')
+def getTransactionsByClientName():
+    return ClientHandler().getTransactionsByClientID()
+
+@app.route('/client/<c_id>/requests')
+def getRequestsByClientName():
+    return ClientHandler().getRequestsByClientID()
+
+@app.route('/client/<c_id>/creditcard')
+def getCreditCardsByClientID():
+    return ClientHandler().getCreditCardsByClientID()
 
 if __name__== '__main__':
     app.run()
