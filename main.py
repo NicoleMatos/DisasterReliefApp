a41933968c703c811a97743db341e8b7ed8465e7
NicoleMatos <nicole.matos3@upr.edu>
2017-12-02 11:09:29 -0400

    Working getAllSuppliers

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify
+from handler.supplier import SupplierHandler
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return 'Disaster Relief'
 
+
+@app.route('/suppliers/')
+def getAllSuppliers():
+    return SupplierHandler().getAllSuppliers()
+
+
 if __name__== '__main__':
     app.run()
+
