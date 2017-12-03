ba16dd43544387bf1c565129aca2067827413bfb
Gilissa Matos <gilissa.matos@upr.edu>
2017-12-03 10:13:13 -0400

    Solucion para main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from handler.supplier import SupplierHandler
 from handler.client import ClientHandler
 from handler.transaction import TransactionHandler
@@ -7,8 +7,6 @@ from handler.request import RequestHandler
 from handler.resource import ResourceHandler
 from handler.ccard import CCardHandler
 from handler.user import UserHandler
-
-
 
 app = Flask(__name__)
 
@@ -20,72 +18,56 @@ def home():
 
 @app.route('/suppliers/')
 def getAllSuppliers():
+    name = request.args.get('name')
+    lastname = request.args.get('lastname')
+    if name:
+        return SupplierHandler().getSupplierByName(name)
+    if lastname:
+        return SupplierHandler().getSupplierByNameAndLastName(name, lastname)
     return SupplierHandler().getAllSuppliers()
+
 
 @app.route('/clients/')
 def getAllClients():
     return ClientHandler().getAllClients()
 
+
 @app.route('/transaction/')
 def getAllTransactions():
     return TransactionHandler().getAllTransactions()
+
 
 @app.route('/announcements/')
 def getAllAnnouncements():
     return AnnouncementHandler().getAllAnnouncements()
 
+
 @app.route('/requests/')
 def getAllRequests():
     return RequestHandler().getAllRequests()
+
 
 @app.route('/resources/')
 def getAllResources():
     return ResourceHandler().getAllResources()
 
+
 @app.route('/creditcards/')
 def getAllCCards():
     return CCardHandler().getAllCCards()
+
 
 @app.route('/users/')
 def getAllUsers():
     return UserHandler().getAllUsers()
 
-@app.route('/client/<int:c_id>/')
-def getClientByID():
-    return ClientHandler().getClientByID()
 
-@app.route('/client/<int:c_id>/suppliers')
-def getSuppliersByClientID():
-    return ClientHandler().getSuppliersByClientID()
 
-@app.route('/client/<int:c_id>/transastions')
-def getTransactionsByClientID():
-    return ClientHandler().getTransactionsByClientID()
+@app.route('/suppliers/<int:s_id>/')
+def getSupplierByID(s_id):
+    return SupplierHandler().getSupplierByID(s_id)
 
-@app.route('/client/<int:c_id>/requests')
-def getRequestsByClientID():
-    return ClientHandler().getRequestsByClientID()
 
-@app.route('/client/<int:c_id>/creditcard')
-def getCreditCardsByClientID():
-    return ClientHandler().getCreditCardsByClientID()
 
-@app.route('/client/<c_name>/suppliers')
-def getSuppliersByClientName():
-    return ClientHandler().getSuppliersByClientID()
-
-@app.route('/client/<c_name>/transastions')
-def getTransactionsByClientName():
-    return ClientHandler().getTransactionsByClientID()
-
-@app.route('/client/<c_id>/requests')
-def getRequestsByClientName():
-    return ClientHandler().getRequestsByClientID()
-
-@app.route('/client/<c_id>/creditcard')
-def getCreditCardsByClientID():
-    return ClientHandler().getCreditCardsByClientID()
-
-if __name__== '__main__':
-    app.run()
-
+if __name__ == '__main__':
+app.run()
