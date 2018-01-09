2a8e1ccf26b7f140b315659c4be4a24f4e3e71bd
Gilissa Matos <gilissa.matos@upr.edu>
2018-01-08 23:06:28 -0400

    announcements

--- handler/announcement.py
+++ handler/announcement.py
@@ -1,84 +1,67 @@
 from flask import jsonify
-
+from dao.announcement import AnnouncementDAO
 
 class AnnouncementHandler:
 
-    def announcement(self):
-        result = [
-            {
-                'a_id': 1,
-                's_id': 1,
-                'a_price': 5.00,
-                'a_date': '02/13/2017',
-                'a_sold_out': 'yes',
-                'a_initial_qty': 4,
-                'a_curr_qty': 4
-            },
-            {
-                'a_id': 2,
-                's_id': 2,
-                'a_price': 10.00,
-                'a_date': '02/20/2017',
-                'a_sold_out': 'no',
-                'a_initial_qty': 4,
-                'a_curr_qty': 2
-            }
-        ]
-        return result
+    def build_announcement_dict(self, row):
+            result = {}
+            result['a_id'] = row[0]
+            result['s_id'] = row[1]
+            result['r_id'] = row[2]
+            result['a_price'] = row[3]
+            result['a_date'] = row[4]
+            result['a_sold_out'] = row[5]
+            result['a_initial_qty'] = row[6]
+            result['a_curr_qty'] = row[7]
+            return result
 
-    # ===================================================================================================================
-    #                                           Search for announcements
-    # ===================================================================================================================
+# ===================================================================================================================
+#                                        search for announcements
+# ===================================================================================================================
 
     def searchAnnouncements(self, args):
-        supplier = args.get('supplier')
-        date = args.get('date')
-        result = []
-        if date and supplier:
-            result = self.getAnnouncementsBySupplierAndDate(supplier, date)
-        elif supplier:
-            result = self.getAnnouncementsBySupplier(supplier)
-        elif date:
-            result = self.getAnnouncementsByDate(date)
-        if len(result) == 0:
-            return jsonify(Error="Announcement Not Found"), 404
-        return jsonify(Result=result)
+            supplier = args.get('s_id')
+            date = args.get('a_date')
+            region = args.get('region')
+            dao = AnnouncementDAO()
+            if (len(args) == 2) and supplier and date:
+                announcement_list = dao.getAnnouncementsBySupplierAndDate(supplier,date)
+            elif (len(args) == 1) and supplier:
+                announcement_list = dao.getAnnouncementsBySupplier(supplier)
+            elif (len(args) == 1) and date:
+                announcement_list = dao.getAnnouncementsByDate(date)
+            elif (len(args) == 1) and region:
+                announcement_list = dao.getAnnouncementByRegion(region)
+            else:
+                return jsonify(Error="Malformed query string"), 400
+            result_list = []
+            for row in announcement_list:
+                result = self.build_announcement_dict(row)
+                result_list.append(result)
+            return jsonify(Announcement=result_list)
 
-    # ===================================================================================================================
-    #                                          get all announcements
-    # ===================================================================================================================
+# ===================================================================================================================
+#                                           get all announcements
+# ===================================================================================================================
 
     def getAllAnnouncements(self):
-        return jsonify(Announcements=self.announcement())
+            dao = AnnouncementDAO()
+            resources_list = dao.getAllAnnouncements()
+            result_list = []
+            for row in resources_list:
+                result = self.build_announcement_dict(row)
+                result_list.append(result)
+            return jsonify(Announcement=result_list)
 
-    # ===================================================================================================================
-    #                                         get announcements by ID
-    # ===================================================================================================================
+# ===================================================================================================================
+#                                           get announcement by ID
+# ===================================================================================================================
 
     def getAnnouncementByID(self, a_id):
-        ann = self.announcement()
-        result = list(filter(lambda announcement: announcement['a_id'] == a_id, ann))
-        if len(result) > 0:
-            return jsonify(Result=result)
-        return jsonify(Error="Announcement Not Found"), 404
-
-
-
-    # ===================================================================================================================
-    #                                         get announcements by Date
-    # ===================================================================================================================
-
-    def getAnnouncementsByDate(self, date):
-        ann = self.announcement()
-        result = list(filter(lambda announcement: announcement['a_date'] == date, ann))
-        return result
-
-    # ===================================================================================================================
-    #                                   get announcements by Supplier And Date
-    # ===================================================================================================================
-
-    def getAnnouncementsBySupplierAndDate(self, supplier, date):
-        ann = self.announcement()
-        result = list(filter(lambda announcement: announcement['s_id'] == supplier, ann))
-        result2 = list(filter(lambda announcement: announcement['a_date'] == date, result))
-        return result2
+            dao = AnnouncementDAO()
+            row = dao.getAnnouncementByID(a_id)
+            if not row:
+                return jsonify(Error="Announcement Not Found"), 404
+            else:
+                result = self.build_announcement_dict(row)
+            return jsonify(Announcement=result)
