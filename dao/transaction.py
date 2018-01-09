f357ff5504a05654eb0662f3ec8633fbea3c54db
NicoleMatos <nicole.matos3@upr.edu>
2018-01-08 23:55:22 -0400

    transactions

---
+++ dao/transaction.py
@@ -0,0 +1,102 @@
+from config.dbconfig import pg_config
+import psycopg2
+
+
+class TransactionDAO:
+    def __init__(self):
+
+        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
+                                                            pg_config['user'],
+                                                            pg_config['passwd'])
+        self.conn = psycopg2._connect(connection_url)
+
+    # ===================================================================================================================
+    #                                           get all suppliers
+    # ===================================================================================================================
+
+    def getAllTransactions(self):
+        cursor = self.conn.cursor()
+        query = "select * from transaction;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    # ===================================================================================================================
+    #                                           get suppliers by things
+    # ===================================================================================================================
+
+    def getTransactionsByDateAndSupplierAndClient(self, date, supplier, client):
+        cursor = self.conn.cursor()
+        query = "select * from transaction where t_date = %s and s_id = %s and c_id = %s;"
+        cursor.execute(query, (date, supplier, client))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getTransactionsByDateAndSupplier(self, date, supplier):
+        cursor = self.conn.cursor()
+        query = "select * from transaction where t_date = %s and s_id = %s;"
+        cursor.execute(query, (date, supplier))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getTransactionsByDateAndClient(self, date, client):
+        cursor = self.conn.cursor()
+        query = "select * from transaction where t_date = %s and c_id = %s;"
+        cursor.execute(query, (date, client))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getTransactionsBySupplierAndClient(self, supplier, client):
+        cursor = self.conn.cursor()
+        query = "select * from transaction where s_id = %s and c_id = %s;"
+        cursor.execute(query, (supplier, client))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getTransactionsByDate(self, date):
+        cursor = self.conn.cursor()
+        query = "select * from transaction where t_date = %s;"
+        cursor.execute(query, (date,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getTransactionsBySupplier(self, supplier):
+        cursor = self.conn.cursor()
+        query = "select * from transaction where s_id = %s;"
+        cursor.execute(query, (supplier,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getTransactionsByClient(self, client):
+        cursor = self.conn.cursor()
+        query = "select * from transaction where c_id = %s;"
+        cursor.execute(query, (client,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    # ===================================================================================================================
+    #                                           get things by id
+    # ===================================================================================================================
+
+    def getTransactionById(self, tid):
+        cursor = self.conn.cursor()
+        query = "select * from transaction where t_id = %s;"
+        cursor.execute(query, (tid,))
+        result = cursor.fetchone()
+        return result
