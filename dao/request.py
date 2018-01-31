111ed8a13d3e3c6d2d6acc992f7a80467335f96d
NicoleMatos <34130792+NicoleMatos@users.noreply.github.com>
2018-01-31 16:32:14 -0400

    Update request.py

--- dao/request.py
+++ dao/request.py
@@ -5,11 +5,9 @@ import psycopg2
 class RequestDAO:
     def __init__(self):
 
-        connection_url = "dbname=%s user=%s password=%s host=%s port=%s" % (pg_config['dbname'],
+        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
                                                             pg_config['user'],
-                                                            pg_config['passwd'],
-                                                            pg_config['host'],
-                                                            pg_config['port'])
+                                                            pg_config['passwd'])
         self.conn = psycopg2._connect(connection_url)
 
     # ===================================================================================================================
@@ -69,6 +67,15 @@ class RequestDAO:
         cursor = self.conn.cursor()
         query = "select * from request where req_date = %s;"
         cursor.execute(query, (date,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getRequestsByRegion(self, region):
+        cursor = self.conn.cursor()
+        query = "select * from request natural inner join client where u_region = %s;"
+        cursor.execute(query, (region,))
         result = []
         for row in cursor:
             result.append(row)
