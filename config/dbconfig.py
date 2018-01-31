1cfdb808cf15e7750bbf06f8c0a73e0e0f7ca514
NicoleMatos <34130792+NicoleMatos@users.noreply.github.com>
2018-01-31 11:32:39 -0400

    Update dbconfig.py

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,7 +1,9 @@
 #Databse configuration information
 
 pg_config = {
-    'user' : 'appusr',
-    'passwd' : 'password',
-    'dbname' : 'appdb'
+    'user' : 'xanimdyubrhzak',
+    'passwd' : '86bb673f96589a5fc2a62773e1d4819bce16e372075ab7c1aa3e2c3927570004',
+    'dbname' : 'das97pufhtobno',
+    'host' : 'ec2-54-163-236-188.compute-1.amazonaws.com',
+    'port' : '5432'
 }
