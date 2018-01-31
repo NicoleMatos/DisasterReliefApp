935e3ca66caa99213f9e55be9bfba610d237ed6b
NicoleMatos <34130792+NicoleMatos@users.noreply.github.com>
2018-01-31 11:38:10 -0400

    Update client.py

--- dao/client.py
+++ dao/client.py
@@ -5,9 +5,11 @@ import psycopg2
 class ClientDAO:
     def __init__(self):
 
-        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
+        connection_url = "dbname=%s user=%s password=%s host=%s port=%s" % (pg_config['dbname'],
                                                             pg_config['user'],
-                                                            pg_config['passwd'])
+                                                            pg_config['passwd'],
+                                                            pg_config['host'],
+                                                            pg_config['port'])
 
         self.conn = psycopg2._connect(connection_url)
 
